eeae043356f7d6e1a6ec26cff37b8cd766aad7c6
GifftyCode <ulokangozi@yahoo.com>
2025-04-29 18:56:05 +0100

    JS logic

--- Guessing Game/script.js
+++ Guessing Game/script.js
@@ -1,1 +1,52 @@
 'use strict';
+
+let secretNumber = Math.trunc(Math.random() * 20 + 1);
+let score = 20;
+let highscore = 0;
+
+document.querySelector('.check').addEventListener('click', function () {
+  let guessedNumber = Number(document.querySelector('.guess').value);
+
+  if (!guessedNumber) {
+    document.querySelector('.message').textContent = '❌ No Number Entered!';
+  } else if (guessedNumber > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '⤴ Number is too High!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.score').textContent = 0;
+      document.querySelector('.message').textContent = '☹️ You lost the game';
+    }
+  } else if (guessedNumber < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '⤵ Number is too Low!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.score').textContent = 0;
+      document.querySelector('.message').textContent = '☹️ You lost the game';
+    }
+  } else if (guessedNumber === secretNumber) {
+    document.querySelector('.number').textContent = secretNumber;
+    document.querySelector('.number').style.width = '30rem';
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.message').textContent = '🎉 Correct Number!!!';
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
+  }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20 + 1);
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+});
