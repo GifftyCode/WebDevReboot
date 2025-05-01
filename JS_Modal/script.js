5c8d30c08cf133b320b7c31b352cffb3d07c0779
GifftyCode <ulokangozi@yahoo.com>
2025-05-01 10:42:05 +0100

    model logic implemented

--- JS_Modal/script.js
+++ JS_Modal/script.js
@@ -1,1 +1,22 @@
 'use strict';
+
+const showModal = document.querySelectorAll('.show-modal');
+const closeModal = document.querySelector('.close-modal');
+const overlay = document.querySelector('.overlay');
+const modal = document.querySelector('.modal');
+
+const modalDisplay = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+const modalRemoved = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < showModal.length; i++) {
+  showModal[i].addEventListener('click', modalDisplay);
+}
+close - modal.addEventListener('click', modalRemoved);
+
+overlay.addEventListener('click', modalRemoved);
